ffbfdcc011f2d35d36082830542ac976d398ed8b
Evdokimov27 <68519208+Evdokimov27@users.noreply.github.com>
2025-07-29 20:56:36 +0400

    work upd

---
+++ Work/Work/SerializableCookie.cs
@@ -0,0 +1,10 @@
+public class SerializableCookie
+{
+	public string Name { get; set; }
+	public string Value { get; set; }
+	public string Domain { get; set; }
+	public string Path { get; set; }
+	public DateTime? Expiry { get; set; }
+	public bool Secure { get; set; }
+	public bool IsHttpOnly { get; set; }
+}
